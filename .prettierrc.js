ad789c22852c442e4748eb38582cd84b2b8e6dda
Vadim Lobanov <vadim@lobanov.pw>
2023-08-09 10:57:52 +0200

    init

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  printWidth: 80,
+  tabWidth: 2,
+  bracketSpacing: false,
+}
---
+++ src/renderer/preload.d.ts
@@ -0,0 +1,10 @@
+import {type ElectronHandler} from 'main/preload';
+
+declare global {
+  // eslint-disable-next-line no-unused-vars
+  interface Window {
+    electron: ElectronHandler;
+  }
+}
+
+export {};
