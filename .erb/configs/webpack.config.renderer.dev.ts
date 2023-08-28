b1098d3817aaba52603181b2c09b7b075704a94d
Vadim Lobanov <vadim@lobanov.pw>
2023-08-28 19:11:37 +0200

    hopefully fix styles

--- .erb/configs/webpack.config.renderer.dev.ts
+++ .erb/configs/webpack.config.renderer.dev.ts
@@ -75,13 +75,27 @@ const configuration: webpack.Configuration = {
             },
           },
           'sass-loader',
-          'postcss-loader'
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [require('tailwindcss'), require('autoprefixer')],
+              },
+            },
+          },
         ],
         include: /\.module\.s?(c|a)ss$/,
       },
       {
         test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+        use: ['style-loader', 'css-loader', 'sass-loader', {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [require('tailwindcss'), require('autoprefixer')],
+            },
+          },
+        }],
         exclude: /\.module\.s?(c|a)ss$/,
       },
       // Fonts
